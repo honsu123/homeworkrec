067e9894ddcdd164a1706dab0bf13b8ec2285454
kirill <nicknamekirill@gmail.com>
2023-02-22 21:54:08 +0300

    masha

---
+++ Desktop/не смотреть/study/c#/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("УРА это же маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
